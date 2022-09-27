3afd815baf651fe188cef123f7810df361e01747
angelherce <angel.herce@imatia.com>
2022-09-27 15:18:31 +0200

    Seccion 14 - Video 102: LivenessProbe con HTTP - Se ha creado un manifiesto de Kubernetes para crear un Pod configurado con un liveness Probe por HTTP. - Se ha creado un script de Linux con los comandos que se han utilizado - Se ha creado un fichero MarkDown para documentar el trabajo realizado en este video

---
+++ seccion-14/102-liveness-http/script.sh
@@ -0,0 +1,9 @@
+kubectl apply -f ./k8s/pod.yaml
+
+kubectl get pod liveness-exec
+
+kubectl describe pod liveness-exec
+
+kubectl get pod liveness-exec -o yaml | grep -i livenessProbe: -A 12
+
+kubectl delete -f ./k8s/pod.yaml
