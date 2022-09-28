31397259e2094b0b8b60cc1bf3a66ae25f70d987
angelherce <angel.herce@imatia.com>
2022-09-28 08:23:23 +0200

    Se ha corregido algunos errores en el script.sh del video 110

--- seccion-15/110-configmap-volume/script.sh
+++ seccion-15/110-configmap-volume/script.sh
@@ -3,8 +3,8 @@ kubectl apply -f ./k8s/config.yaml -f ./k8s/deployment.yaml
 #kubectl get configmap
 kubectl get cm
 
-#kubectl describe configmap nginx-config
-kubectl describe cm nginx-config
+#kubectl describe configmap nginx-conf
+kubectl describe cm nginx-conf
 
 kubectl get pod
 
