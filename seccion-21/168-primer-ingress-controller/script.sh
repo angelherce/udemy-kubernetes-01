e692702c14743974bfde17ae789182cbc923cc8c
angelherce <angel.herce@imatia.com>
2022-11-07 13:18:22 +0100

    Seccion 21 - Video 168: Crea tu primer Ingress Controller - Se han corregido algunos errores

--- seccion-21/168-primer-ingress-controller/script.sh
+++ seccion-21/168-primer-ingress-controller/script.sh
@@ -1,7 +1,7 @@
-minikube addons enable ingress
-
 kubectl apply -f ./k8s/nginx-controller.yaml
+minikube addons enable ingress
 
 kubectl -n ingress-nginx get pod
 
 kubectl delete -f ./k8s/nginx-controller.yaml
+minikube addons disable ingress
