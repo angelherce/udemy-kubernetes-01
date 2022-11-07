e20de90e1de3f90bc4427da6133a3752434a331f
angelherce <angel.herce@imatia.com>
2022-11-07 13:18:48 +0100

    Seccion 21 - Video 171: Crea reglas en el Ingress para exponer tu app - Se han corregido algunos errores

--- seccion-21/171-ingress-rules/script.sh
+++ seccion-21/171-ingress-rules/script.sh
@@ -1,6 +1,6 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.4.0/deploy/static/provider/baremetal/deploy.yaml
 minikube addons enable ingress
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.4.0/deploy/static/provider/baremetal/deploy.yaml
 kubectl apply -f ./k8s/deployment.yaml
 kubectl apply -f ./k8s/service.yaml
 kubectl apply -f ./k8s/ingress-rules.yaml
@@ -12,4 +12,6 @@ kubectl get ingress
 kubectl delete -f ./k8s/ingress-rules.yaml
 kubectl delete -f ./k8s/service.yaml
 kubectl delete -f ./k8s/deployment.yaml
+
 kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.4.0/deploy/static/provider/baremetal/deploy.yaml
+minikube addons disable ingress
