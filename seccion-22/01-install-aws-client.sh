51074115a01464db5b9b8c10986310b2bc9d8884
angelherce <angel.herce@imatia.com>
2022-11-08 08:50:49 +0100

    Seccion 22 - Video 176: Instala AWS CLI

---
+++ seccion-22/01-install-aws-client.sh
@@ -0,0 +1,35 @@
+# INSTALL AWS CLIENT
+
+# Install AWS CLI con PIP
+# ---------------------------------------------- #
+
+# 1- APT Update
+sudo apt update
+
+# 2- Install PIP in Ubuntu 22.04.1
+sudo apt install python3-pip
+
+# 3- Check PIP instalation
+pip3 --version
+
+# 4- Install
+pip3 install -U awsCli
+
+# 5- Check AWS CLI instalation
+aws --version
+
+# ---------------------------------------------- #
+
+
+
+# Install AWS CLI sin PIP
+# ---------------------------------------------- #
+
+# 1- Install
+sudo apt install awscli
+
+# 2- Check AWS CLI instalation
+aws --version
+
+
+# ---------------------------------------------- #
